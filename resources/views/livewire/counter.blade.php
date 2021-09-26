f0a6c542e550fce304647c5e34c08cf20186f8ec
MrAlhadi <m.m.h.alhadi@gmail.com>
2021-09-26 20:47:27 +0200

    first commit

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div style="text-align: center">
+    {{-- Be like water. --}}
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+    <button wire:click="decrement">-</button>
+</div>
